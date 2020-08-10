c40dc24775d6c95c5ef8c4dfb8afb25ac4b14908
Adarah <lucasyharada@gmail.com>
2020-08-10 02:36:49 -0300

    added Games struct, and several debug messages

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -4,15 +4,52 @@ use wasm_bindgen::prelude::JsValue;
 // might want to use a struct or hashmap of games instead?
 pub const TETRIS: &'static [u8] = include_bytes!("TETRIS");
 pub const BRIX: &'static [u8] = include_bytes!("BRIX");
+pub const PONG: &'static [u8] = include_bytes!("PONG");
+pub const SCTEST: &'static [u8] = include_bytes!("test_ROMs/SCTEST.ch8");
+pub const BCTEST: &'static [u8] = include_bytes!("test_ROMs/BC_test.ch8");
 
-pub fn parse_title(title: JsValue) -> Result<&'static [u8], JsValue> {
-    if let Some(title) = title.as_string() {
-        let bytes = match title.to_lowercase().as_str() {
-            "tetris" => TETRIS,
-            "brix" => BRIX,
+pub struct Game {
+    pub code: &'static [u8],
+    pub load_store_quirk: bool,
+    pub shift_quirk: bool,
+}
+
+impl Game {
+    pub fn new(title: &str) -> Result<Self, JsValue> {
+        let game = match title.to_lowercase().as_str() {
+            "tetris" => Game {
+                code: TETRIS,
+                load_store_quirk: true,
+                shift_quirk: true,
+            },
+            "brix" => Game {
+                code: BRIX,
+                load_store_quirk: true,
+                shift_quirk: true,
+            },
+            "pong" => Game {
+                code: PONG,
+                load_store_quirk: true,
+                shift_quirk: true,
+            },
+            "sctest" => Game {
+                code: SCTEST,
+                load_store_quirk: true,
+                shift_quirk: true,
+            },
+            "bctest" => Game {
+                code: BCTEST,
+                load_store_quirk: true,
+                shift_quirk: true,
+            },
             _ => return Err(Error::new("unknown game chosen").into()),
         };
-        return Ok(&bytes);
+        Ok(game)
     }
-    Err(Error::new("Could not parse title as string").into())
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Game { code: BCTEST, load_store_quirk: true, shift_quirk: true }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 #![feature(slice_fill)]
+
+pub mod games;
+mod utils;
+
+use games::Game;
 use js_sys::Error;
 use rand::{thread_rng, Rng};
 use wasm_bindgen::prelude::*;
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 // mod memory;
-pub mod games;
-mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,17 +31,13 @@ const NEW_FRAME_START: usize = MEMORY_SIZE - DISPLAY_BUFFER_SIZE / 2;
 const STACK_START: usize = MEMORY_SIZE - DISPLAY_BUFFER_SIZE;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 pub struct Chip8 {
     memory: [u8; MEMORY_SIZE],
     registers: RegisterBank,
     pc: usize,
     sp: usize,
     keypad: [bool; 16],
+    game: Game,
 }
 
 struct RegisterBank {
@@ -76,9 +83,10 @@ impl Default for Chip8 {
         Chip8 {
             memory,
             registers: RegisterBank::default(),
-            pc: 0,
+            pc: 0x200,
             sp: STACK_START,
             keypad: [false; 16],
+            game: Game::default(),
         }
     }
 }
@@ -95,7 +103,8 @@ impl Chip8 {
     }
 
     pub fn display_buffer_ptr(&self) -> *const u8 {
-        self.memory[STACK_START..NEW_FRAME_START].as_ptr()
+        log!("{:?}", &self.memory[STACK_START..NEW_FRAME_START]);
+        self.memory[NEW_FRAME_START..].as_ptr()
     }
 
     pub fn display_buffer_size(&self) -> usize {
@@ -126,19 +135,31 @@ impl Chip8 {
         Ok(())
     }
 
-    pub fn load_rom(&mut self, game: JsValue) -> Result<(), JsValue> {
-        let game_data = games::parse_title(game)?;
-        for (mem, bytes) in self.memory.iter_mut().skip(0x200).zip(game_data) {
-            *mem = *bytes;
+    pub fn load_rom(&mut self, title: JsValue) -> Result<(), JsValue> {
+        match title.as_string() {
+            None => Err(Error::new("Could not parse title as string").into()),
+            Some(title) => {
+                let game = games::Game::new(&title)?;
+
+                for (mem, bytes) in self.memory.iter_mut().skip(0x200).zip(game.code) {
+                    *mem = *bytes;
+                }
+                self.sp = STACK_START;
+                self.pc = 0x200;
+                self.game = game;
+                Ok(())
+            }
         }
-        self.sp = STACK_START;
-        self.pc = 0x200;
-        Ok(())
     }
 
     pub fn tick(&mut self) -> Result<(), JsValue> {
         let machine_code = self.fetch();
-        self.decode_and_execute(machine_code).into()
+        self.decode_and_execute(machine_code)
+    }
+
+    pub fn decrement_timers(&mut self) {
+        self.registers.delay = self.registers.delay.saturating_sub(1);
+        self.registers.sound = self.registers.sound.saturating_sub(1);
     }
 
     fn fetch(&mut self) -> u16 {
@@ -148,6 +169,7 @@ impl Chip8 {
     }
 
     fn decode_and_execute(&mut self, instruction_code: u16) -> Result<(), JsValue> {
+        log!("instruction_code: {:04X?}", instruction_code);
         let nibbles = (
             (instruction_code >> 12 & 0x000F),
             (instruction_code >> 8 & 0x000F) as usize,
@@ -174,9 +196,9 @@ impl Chip8 {
             (0x8, x, _, 3) => self.xor(x, vy),
             (0x8, x, _, 4) => self.add_registers(x, vy),
             (0x8, x, _, 5) => self.sub_vy_from_vx(x, vy),
-            (0x8, x, _, 6) => self.shift_right(x), // logical right shift, not arithmetic
+            (0x8, x, y, 6) => self.shift_right(x, y), // logical right shift, not arithmetic
             (0x8, x, _, 7) => self.sub_vx_from_vy(x, vy),
-            (0x8, x, _, 0xE) => self.shift_left(x),
+            (0x8, x, y, 0xE) => self.shift_left(x, y),
             (0x9, _, _, 0) => self.skip_next_if_not_equal_to_register(vx, vy),
             (0xA, _, _, _) => self.set_I(triple),
             (0xB, _, _, _) => self.jump_relative(triple),
@@ -199,6 +221,7 @@ impl Chip8 {
 
     fn set_sp(&mut self, offset: isize) -> Result<(), JsValue> {
         self.sp = (self.sp as isize + offset) as usize;
+        log!("set stack pointer to: {}", self.sp);
         if !(STACK_START - 12 * 2 < self.sp && self.sp < STACK_START) {
             return Err(Error::new("StackOverflow: Stack exceeded maximum size").into());
         }
@@ -206,14 +229,16 @@ impl Chip8 {
     }
 
     fn set_pc(&mut self, address: usize) -> Result<(), JsValue> {
-        self.sp = address;
-        if self.sp % 2 != 0 {
+        self.pc = address;
+        log!("set program counter to: {:04X}", self.pc);
+        if self.pc % 2 != 0 {
             return Err(Error::new("AlignementError: Unaligned memory access attempted!").into());
         }
         Ok(())
     }
 
     fn clear_display(&mut self) -> Result<(), JsValue> {
+        log!("Clearing display");
         let (old_frame, current_frame) = self.memory.split_at_mut(NEW_FRAME_START);
         old_frame[STACK_START..NEW_FRAME_START].copy_from_slice(current_frame);
         current_frame.fill(0);
@@ -222,15 +247,18 @@ impl Chip8 {
 
     fn return_from_subroutine(&mut self) -> Result<(), JsValue> {
         self.pc = (self.memory[self.sp] as usize) << 8 | self.memory[self.sp + 1] as usize;
+        log!("Returning from subroutine: {:04X}", self.sp);
         self.set_sp(2)?;
         Ok(())
     }
 
     fn jump(&mut self, address: usize) -> Result<(), JsValue> {
+        log!("Jumping to: {:04X}", address);
         self.set_pc(address)
     }
 
     fn call_subroutine(&mut self, address: usize) -> Result<(), JsValue> {
+        log!("Calling subroutine: {:04X}", address);
         self.set_sp(-2)?;
         self.memory[self.sp] = (address & 0xFF00) as u8;
         self.memory[self.sp + 1] = (address & 0x00FF) as u8;
@@ -238,6 +266,7 @@ impl Chip8 {
     }
 
     fn skip_next_if_equal_to_byte(&mut self, vx: u8, byte: u8) -> Result<(), JsValue> {
+        log!("Skip next if vx == byte: ({}, {})", vx, byte);
         if vx == byte {
             self.pc += 2;
         }
@@ -245,6 +274,7 @@ impl Chip8 {
     }
 
     fn skip_next_if_not_equal_to_byte(&mut self, vx: u8, byte: u8) -> Result<(), JsValue> {
+        log!("Skip next if vx != byte: ({}, {})", vx, byte);
         if vx != byte {
             self.pc += 2;
         }
@@ -252,6 +282,7 @@ impl Chip8 {
     }
 
     fn skip_next_if_equal_to_register(&mut self, vx: u8, vy: u8) -> Result<(), JsValue> {
+        log!("Skip next if vx == vy: ({}, {})", vx, vy);
         if vx == vy {
             self.pc += 2;
         }
@@ -259,36 +290,44 @@ impl Chip8 {
     }
 
     fn load_from_byte(&mut self, x: usize, byte: u8) -> Result<(), JsValue> {
+        log!("Loading V{:X} with {}", x, byte);
         self.registers.Vx[x] = byte;
         Ok(())
     }
 
     fn add_byte(&mut self, x: usize, byte: u8) -> Result<(), JsValue> {
-        self.registers.Vx[x] += byte;
+        log!("Adding V{:X} with {}", x, byte);
+        self.registers.Vx[x] = self.registers.Vx[x].wrapping_add(byte);
         Ok(())
     }
 
     fn load_from_register(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
+        log!("Load V{:X} with {}", x, vy);
         self.registers.Vx[x] = vy;
         Ok(())
     }
 
     fn or(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
+        log!("ORing V{:X} with {}", x, vy);
         self.registers.Vx[x] |= vy;
         Ok(())
     }
 
     fn and(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
+        log!("ANDing V{:X} with {}", x, vy);
         self.registers.Vx[x] &= vy;
         Ok(())
     }
 
     fn xor(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
+        log!("XORing V{:X} with {}", x, vy);
         self.registers.Vx[x] ^= vy;
         Ok(())
     }
 
     fn add_registers(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
+        log!("Adding V{:X} with {}", x, vy);
+
         self.registers.Vx[0xF] = 0;
 
         self.registers.Vx[x] = self.registers.Vx[x].checked_add(vy).unwrap_or_else(|| {
@@ -299,40 +338,62 @@ impl Chip8 {
     }
 
     fn sub_vy_from_vx(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
-        if self.registers.Vx[x] > vy {
+        log!("Subbingg {} from V{:X}", vy, x);
+        if self.registers.Vx[x] >= vy {
             self.registers.Vx[0xF] = 1;
         } else {
             self.registers.Vx[0xF] = 0;
         }
 
-        self.registers.Vx[x] -= vy;
+        self.registers.Vx[x] = self.registers.Vx[x].wrapping_sub(vy);
         Ok(())
     }
 
-    fn shift_right(&mut self, x: usize) -> Result<(), JsValue> {
-        self.registers.Vx[0xF] = self.registers.Vx[x] & 0b0001;
-        self.registers.Vx[x] = self.registers.Vx[x] >> 1;
+    fn shift_right(&mut self, x: usize, y: usize) -> Result<(), JsValue> {
+        log!("Right shifting V{:X}", x);
+        if self.game.shift_quirk {
+            self.registers.Vx[0xF] = self.registers.Vx[x] & 1;
+            self.registers.Vx[x] = self.registers.Vx[x] >> 1;
+        } else {
+            self.registers.Vx[0xF] = self.registers.Vx[y] & 1;
+            self.registers.Vx[x] = self.registers.Vx[y] >> 1;
+        }
         Ok(())
     }
 
     fn sub_vx_from_vy(&mut self, x: usize, vy: u8) -> Result<(), JsValue> {
-        if vy > self.registers.Vx[x] {
+        log!("Subbing V{:X} from {}", x, vy);
+        if vy >= self.registers.Vx[x] {
             self.registers.Vx[0xF] = 1;
         } else {
             self.registers.Vx[0xF] = 0;
         }
 
-        self.registers.Vx[x] = vy - self.registers.Vx[x];
+        self.registers.Vx[x] = vy.wrapping_sub(self.registers.Vx[x]);
         Ok(())
     }
 
-    fn shift_left(&mut self, x: usize) -> Result<(), JsValue> {
-        self.registers.Vx[0xF] = self.registers.Vx[x] & 0b1000;
-        self.registers.Vx[x] = self.registers.Vx[x] << 1;
+    fn shift_left(&mut self, x: usize, y: usize) -> Result<(), JsValue> {
+        log!("Left shifting V{:X}", x);
+        // log!("before");
+        // log!("VF: {}", self.registers.Vx[0xF]);
+        // log!("V{:X}: {}", x, self.registers.Vx[x]);
+
+        if self.game.shift_quirk {
+            self.registers.Vx[0xF] = (self.registers.Vx[x] & 0b10000000) >> 7;
+            self.registers.Vx[x] = self.registers.Vx[x] << 1;
+        } else {
+            self.registers.Vx[0xF] = (self.registers.Vx[y] & 0b10000000) >> 7;
+            self.registers.Vx[x] = self.registers.Vx[y] << 1;
+        }
+        // log!("after");
+        // log!("VF: {}", self.registers.Vx[0xF]);
+        // log!("V{:X}: {}", x, self.registers.Vx[x]);
         Ok(())
     }
 
     fn skip_next_if_not_equal_to_register(&mut self, vx: u8, vy: u8) -> Result<(), JsValue> {
+        log!("Skip next if vx != vy ({}, {})", vx, vy);
         if vx != vy {
             self.pc += 2;
         }
@@ -341,21 +402,26 @@ impl Chip8 {
 
     fn set_I(&mut self, address: usize) -> Result<(), JsValue> {
         self.registers.I = address;
+        log!("Setting I: {:04X}", self.registers.I);
         Ok(())
     }
 
     fn jump_relative(&mut self, address: usize) -> Result<(), JsValue> {
-        self.set_pc(address + self.registers.Vx[0] as usize)
+        self.set_pc(address + self.registers.Vx[0] as usize)?;
+        log!("Jumping relative to: {:04X}", self.pc);
+        Ok(())
     }
 
     fn set_random_number(&mut self, x: usize, byte: u8) -> Result<(), JsValue> {
         let mut rng = thread_rng();
         let random_num: u8 = rng.gen();
         self.registers.Vx[x] = random_num & byte;
+        log!("Setting random number: {:02X}", self.registers.Vx[x]);
         Ok(())
     }
 
     fn draw(&mut self, vx: u8, vy: u8, n: u8) -> Result<(), JsValue> {
+        log!("Draw");
         let (rest, current_frame) = self.memory.split_at_mut(NEW_FRAME_START);
         let (rest, smoothed_frame) = rest.split_at_mut(STACK_START);
         smoothed_frame.copy_from_slice(current_frame);
@@ -364,7 +430,7 @@ impl Chip8 {
         let sprite = &rest[self.registers.I..(self.registers.I + n as usize)];
         for x in 0..n {
             let sprite_byte = sprite[x as usize];
-            for y in 0..64 {
+            for y in 0..8 {
                 let row = ((vy + x) % 32) as usize;
                 let col = ((vx + y) % 64) as usize;
                 let byte = (row * 64 + col) / 8;
@@ -384,6 +450,7 @@ impl Chip8 {
     }
 
     fn skip_if_key_is_pressed(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Skip next if key is pressed: {:X}", vx);
         if self.keypad[vx as usize] {
             self.pc += 2;
         }
@@ -391,6 +458,7 @@ impl Chip8 {
     }
 
     fn skip_if_key_is_not_pressed(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Skip next if key is not pressed: {:X}", vx);
         if !self.keypad[vx as usize] {
             self.pc += 2;
         }
@@ -398,11 +466,13 @@ impl Chip8 {
     }
 
     fn load_from_delay_timer(&mut self, x: usize) -> Result<(), JsValue> {
+        log!("Loading with delay timer: {}", self.registers.delay);
         self.registers.Vx[x] = self.registers.delay;
         Ok(())
     }
 
     fn block_until_key_is_pressed(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Block until key is pressed: {}", self.registers.delay);
         if !self.keypad[vx as usize] {
             self.pc -= 2;
         }
@@ -410,26 +480,31 @@ impl Chip8 {
     }
 
     fn set_delay_timer(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Set delay timer: {}", vx);
         self.registers.delay = vx;
         Ok(())
     }
 
     fn set_sound_timer(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Set sound timer: {}", vx);
         self.registers.sound = vx;
         Ok(())
     }
 
     fn increment_I(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Increment I: {}", vx);
         self.registers.I += vx as usize;
         Ok(())
     }
 
     fn load_font_location_in_I(&mut self, vx: u8) -> Result<(), JsValue> {
         self.registers.I = FONT_LOCATION + (vx as usize) * 5;
+        log!("Set I to font location of {}: {}", vx, self.registers.I);
         Ok(())
     }
 
     fn store_BCD(&mut self, vx: u8) -> Result<(), JsValue> {
+        log!("Store BCD: {}", vx);
         let hundreds = vx / 100;
         let tens = (vx % 100) / 10;
         let ones = vx % 10;
@@ -440,14 +515,27 @@ impl Chip8 {
     }
 
     fn bulk_store(&mut self, x: usize) -> Result<(), JsValue> {
-        self.memory[self.registers.I..self.registers.I + x]
-            .copy_from_slice(&self.registers.Vx[0..x]);
+        log!("Bulk store from V0 to V{:X}", x);
+        self.memory[self.registers.I..self.registers.I + x + 1]
+            .copy_from_slice(&self.registers.Vx[0..x + 1]);
+        if !self.game.load_store_quirk {
+            self.registers.I = self.registers.I + x + 1;
+        }
         Ok(())
     }
 
     fn bulk_load(&mut self, x: usize) -> Result<(), JsValue> {
-        self.registers.Vx[0..x]
-            .copy_from_slice(&self.memory[self.registers.I..self.registers.I + x]);
+        log!("Bulk load into V0 to V{:X}", x);
+        self.registers.Vx[0..x + 1]
+            .copy_from_slice(&self.memory[self.registers.I..self.registers.I + x + 1]);
+        log!("registers: {:?}", self.registers.Vx);
+        log!(
+            "memory region: {:?}",
+            &self.memory[self.registers.I..self.registers.I + x + 1]
+        );
+        if !self.game.load_store_quirk {
+            self.registers.I = self.registers.I + x + 1;
+        }
         Ok(())
     }
 }
